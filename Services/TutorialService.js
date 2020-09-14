678f84bde4b2d83ec718e51eb4579280172b331d
unknown <reyesvi92@gmail.com>
2020-09-14 13:33:38 -0300

    Updates

--- Services/TutorialService.js
+++
@@ -1,9 +0,0 @@
-export const fetchTutorials = async () => {
-    const response = await fetch("https://rayentutorialtestapp.azurewebsites.net/tutorials");
-    return await response.json();
-}
-
-export const fetchTutorialByDescription = async (search) => {
-    const response = await fetch(`https://rayentutorialtestapp.azurewebsites.net/tutorial?description=${search}`);
-    return await response.json();
-}
---
+++ precache-manifest.02c81d7b44decbc8aa5f3e859fde68f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e325e59d83350d697198cae11a41f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "865ff5edf1148609d182",
+    "url": "/static/css/main.674f4c56.chunk.css"
+  },
+  {
+    "revision": "3e08a95fa6b73100ff65",
+    "url": "/static/js/2.61dc77cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.61dc77cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "865ff5edf1148609d182",
+    "url": "/static/js/main.c8e705bd.chunk.js"
+  },
+  {
+    "revision": "3ac12f66a908330c7063",
+    "url": "/static/js/runtime-main.dec51524.js"
+  }
+]);
