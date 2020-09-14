367962ce157575a490c6890502b9def17d8d633c
unknown <reyesvi92@gmail.com>
2020-09-14 13:35:37 -0300

    Updates

---
+++ precache-manifest.1f9ba428dacc424a445b621dba083e8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f45a3d9e4aaa51a1f68f73851724234",
+    "url": "/desafio-rayen/index.html"
+  },
+  {
+    "revision": "40824890e9d31fab494d",
+    "url": "/desafio-rayen/static/css/main.674f4c56.chunk.css"
+  },
+  {
+    "revision": "3e08a95fa6b73100ff65",
+    "url": "/desafio-rayen/static/js/2.61dc77cd.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/desafio-rayen/static/js/2.61dc77cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40824890e9d31fab494d",
+    "url": "/desafio-rayen/static/js/main.f3ac52da.chunk.js"
+  },
+  {
+    "revision": "8b27a95b8c8d85c370db",
+    "url": "/desafio-rayen/static/js/runtime-main.38d623b2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02c81d7b44decbc8aa5f3e859fde68f3.js"
+  "/desafio-rayen/precache-manifest.1f9ba428dacc424a445b621dba083e8f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/desafio-rayen/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
